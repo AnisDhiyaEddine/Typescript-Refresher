4b82ab20247492aa07a3e391fec74754d2cde5c8
ANISDHIYAEDDINE BOUDIAF <dhiaeboudiaf@gmail.com>
2021-02-27 20:50:11 +0100

    Api with typescript anotations added

--- expressTs/src/index.ts
+++ expressTs/src/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
 import { router as loginRouter } from './loginRoutes';
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: [ 'cookies key' ] }));
 app.use(loginRouter);
 app.listen(3000, () => console.log('app up and running'));
--- expressTs/src/loginRoutes.ts
+++ expressTs/src/loginRoutes.ts
@@ -1,10 +1,32 @@
-import { Router, Response, Request } from 'express';
+import {
+	Router,
+	Response,
+	Request,
+	NextFunction,
+} from 'express';
 
 interface Request_body extends Request {
 	body: { [key: string]: string | undefined };
 }
 
 const router = Router();
+
+let requireAuth = (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	if (!(req.session && req.session.loggedIn)) {
+		res.status(403).send('haha must login bitch');
+	}
+	next();
+};
+
+router.get('/', (req: Request_body, res: Response) => {
+	if (req.session && req.session.loggedIn)
+		res.send("hello khaoula you're in");
+	res.send('You must login :(');
+});
 
 router.get('/login', (req: Request_body, res: Response) => {
 	res.send(`
@@ -28,7 +50,28 @@ router.post(
 	(req: Request_body, res: Response) => {
 		const { email, password } = req.body;
 
-		res.send({ email, password });
+		if (
+			email === 'hello@khaoula.fr' &&
+			password === 'khaoula <3'
+		) {
+			req.session = { loggedIn: true };
+			res.redirect('/');
+		}
+	},
+);
+
+router.get('/logout', (req: Request, res: Response) => {
+	req.session = undefined;
+	res.redirect('/');
+});
+
+router.get(
+	'/protected',
+	requireAuth,
+	(req: Request, res: Response) => {
+		res.send(
+			"Hello khaoula this is protected and you're the only one who can see it ;)",
+		);
 	},
 );
 export { router };
