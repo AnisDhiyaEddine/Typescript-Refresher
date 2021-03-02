6eb484c35de02ff42056c77ab614b6acf80045cf
ANISDHIYAEDDINE BOUDIAF <dhiaeboudiaf@gmail.com>
2021-03-02 09:05:41 +0100

    typscript integration with react/redux

--- reactTs/src/index.tsx
+++ reactTs/src/index.tsx
@@ -1,38 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducers';
 
-interface AppProps{
-  color: string;
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
-interface AppState{
-  count: number
-}
-class App extends React.Component<AppProps, AppState> {
-  
-  constructor(props: AppProps) {
-    super(props);
-    this.state = {
-      count : 0
-    }
-  }
-  onClickInc = ():void => {
-    this.setState({count:this.state.count + 1})
-  }
-
-  onClickDec = (): void => {
-    this.setState({ count: this.state.count - 1 })
-  }
-   render() {
-     return (
-       <div>
-         <button onClick={this.onClickInc}>Increment</button>
-         <button onClick={this.onClickDec}>Decrement</button>
-         <br />
-         <div>Count is { this.state.count}</div>
-       </div>
-     )
-  }
-}
-
-ReactDOM.render(<App color="red" />,document.querySelector('#root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
