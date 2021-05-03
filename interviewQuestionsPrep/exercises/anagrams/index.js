98e49df32c74d2132929ce35eb80be58b9e7b2c6
ANISDHIYAEDDINE_BOUDIAF <dhiaeboudiaf@gmail.com>
2021-05-03 02:42:58 +0200

    Anagrams added

--- interviewQuestionsPrep/exercises/anagrams/index.js
+++ interviewQuestionsPrep/exercises/anagrams/index.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB){
+    return cleanString(stringA).split('').sort().join('') === cleanString(stringB).split('').sort().join('');
+}
+
+
 
 module.exports = anagrams;
+
+
+
+/* 
+function anagrams(stringA, stringB) {
+    const stringASan = cleanString(stringA);
+    const stringBSan = cleanString(stringB)
+    if(stringASan.length != stringBSan.length) return false;
+    let objA = buildObj(stringASan);
+    let objB = buildObj(stringBSan);
+    for(let key in objA){
+        if(objA[key] != objB[key]) return false;
+    }
+    return true;
+}
+*/
+
+const cleanString = (string) => string.replace('/[^\w]/g','').toLowerCase();
+const buildObj = (string) => {
+let obj = {};
+for(let char of string){
+    obj[char] ? obj[char]++ : obj[char] = 1;
+ }
+ return obj;
+}
